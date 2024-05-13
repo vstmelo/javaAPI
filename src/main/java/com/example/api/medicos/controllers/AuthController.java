36341ff7ea2c50562871afe4c04f4a3018654e55
Vinicius <vstmelo@proton.me>
2024-05-13 08:18:07 -0300

    feat: created controller to authentication

---
+++ src/main/java/com/example/api/medicos/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.api.medicos.controllers;
+
+import com.example.api.medicos.domain.usuarios.DadosAuthParams;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    
+    @PostMapping
+    @Transactional
+    public ResponseEntity AuthLogin(@RequestBody @Valid DadosAuthParams data){
+        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var authentication = authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
+
+    }
+}
---
+++ src/main/java/com/example/api/medicos/domain/usuarios/DadosAuthParams.java
@@ -0,0 +1,8 @@
+package com.example.api.medicos.domain.usuarios;
+
+public record DadosAuthParams(
+        String login,
+        String password
+
+) {
+}
